796e54ea782b2ec9872a8a916effa813e1b8ee0c
Kevin.Beauchemin <kevin.beauchemin@outlook.com>
2020-08-20 15:52:39 -0600

    Updated filepaths for data files located within the working directory

--- run_analysis.R
+++ run_analysis.R
@@ -4,22 +4,22 @@ library(readr)
 library(tidyr)
 
 ## Read feature and activity names and convert to character vectors for easy use
-activityNames <- read.table("./data/UCI HAR Dataset/activity_labels.txt")
+activityNames <- read.table("./UCI HAR Dataset/activity_labels.txt")
 activityNames <- activityNames$V2
 
-featureNames <- read.table("./data/UCI HAR Dataset/features.txt")
+featureNames <- read.table("./UCI HAR Dataset/features.txt")
 featureNames <- featureNames$V2
 
 ## Read all test and training data into tibble data frames
-testData <- as_tibble(read.table("./data/UCI HAR Dataset/test/X_test.txt"))
+testData <- as_tibble(read.table("./UCI HAR Dataset/test/X_test.txt"))
 names(testData) <- featureNames
-testActivity <- as_tibble(read.table("./data/UCI HAR Dataset/test/y_test.txt", col.names = "Activity"))
-testSubject <- as_tibble(read.table("./data/UCI HAR Dataset/test/subject_test.txt", col.names = "SubjectID"))
+testActivity <- as_tibble(read.table("./UCI HAR Dataset/test/y_test.txt", col.names = "Activity"))
+testSubject <- as_tibble(read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = "SubjectID"))
 
-trainData <- as_tibble(read.table("./data/UCI HAR Dataset/train/X_train.txt"))
+trainData <- as_tibble(read.table("./UCI HAR Dataset/train/X_train.txt"))
 names(trainData) <- featureNames
-trainActivity <- as_tibble(read.table("./data/UCI HAR Dataset/train/y_train.txt", col.names = "Activity"))
-trainSubject <- as_tibble(read.table("./data/UCI HAR Dataset/train/subject_train.txt", col.names = "SubjectID"))
+trainActivity <- as_tibble(read.table("./UCI HAR Dataset/train/y_train.txt", col.names = "Activity"))
+trainSubject <- as_tibble(read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = "SubjectID"))
 
 ## Build full test and train tables with activity and subject ids included
 testData <- bind_cols(testSubject, testActivity, testData)
